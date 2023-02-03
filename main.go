43368d7a182855adda154cfd300cd13132fc26ac
Salman <mohd.salman@nurture.farm>
2023-02-03 21:20:22 +0530

    ss

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"GolangDS/stack"
+	"GolangDS/linkedlist"
 	"fmt"
 )
 
 func main() {
-	st := stack.New[byte]()
-	for _, i := range []byte{'a', 'b', 'c', 'd'} {
-		//s := fmt.Sprintf("%v", i)
-		st.Push(i)
+	//st := stack.New[byte]()
+	//for _, i := range []byte{'a', 'b', 'c', 'd'} {
+	//	//s := fmt.Sprintf("%v", i)
+	//	st.Push(i)
+	//}
+	//for st.Empty() == false {
+	//	top := st.Top()
+	//	fmt.Println(top)
+	//	st.Pop()
+	//}
+
+	ll := linkedlist.NewDoublyLinkedList[string]()
+	for i := 1; i < 4; i++ {
+		//ll = ll.AddFront(i)
+		f := fmt.Sprintf("%v_k", i)
+		ll.AddLast(f)
 	}
-	for st.Empty() == false {
-		top := st.Top()
-		fmt.Println(top)
-		st.Pop()
+	temp := ll
+	for ll != nil {
+		fmt.Println(ll.Val)
+		temp = ll
+		ll = ll.Next
+		//if ll.Next != nil{
+
+		//}
 	}
+	for temp != nil {
+		fmt.Println(temp.Val)
+		temp = temp.Previous
+	}
+
 }
--- queue/queue.go
+++ queue/queue.go
@@ -1,1 +1,11 @@
 package queue
+
+import "container/list"
+
+type object interface {
+	int | int32 | int64 | string | byte
+}
+
+type queue[o object] struct {
+	list list.List
+}
